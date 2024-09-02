f3d3cddc2b744f0ab090cdaa2b3543b584d3dd5d
Guðmundur Björn Birkisson <gbb@gbb.is>
2024-09-02 21:23:32 +0200

    fix: fix broken import

--- custom_components/__init__.py
+++
@@ -1,9 +0,0 @@
-import fnmatch
-
-
-def wildcard_filter(all: list[str], patterns: set[str]) -> tuple[set[str], set[str]]:
-    match = set([])
-    for p in patterns:
-        match.update(fnmatch.filter(all, p))
-    no_match = set(all) - match
-    return match, no_match
